c0c6d8c758eb9d5bcefb92868700789dbb1a1660
rmfsantos <111084824+rmfsantos@users.noreply.github.com>
2026-06-16 11:01:02 -0300

    Delete conteudoGC.sh

--- conteudoGC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GC=`grep '^[^>]' $1 | grep -o  '[GC]' | wc -l`
-AT=`grep '^[^>]' $1 | grep -o '[AT]' | wc -l`
-tot=$(($GC+$AT))
-perc=$(($GC*100/$tot))
-echo O percentual GC é de $perc %
-
