13f968907793f51afd9365d36d0c10bf799ac73e
rmfsantos <111084824+rmfsantos@users.noreply.github.com>
2026-07-07 10:53:28 -0300

    Add script to run FastQC on trimmed reads

---
+++ Aula_2/10_run_fastqc_fastp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ENV_NAME="orchid_qc"
+RAW_DIR="data/trimmed_fastp"
+OUT_DIR="results/fastp/fastqc"
+
+echo "Ativando ambiente Conda: ${ENV_NAME}..."
+conda activate "${ENV_NAME}"
+
+echo "Criando diretório de saída do FastQC (fastp): ${OUT_DIR}..."
+mkdir -p "${OUT_DIR}"
+
+echo "Rodando FastQC nas reads pós-fastp..."
+fastqc "${RAW_DIR}"/*.fastq* -o "${OUT_DIR}"
+
+echo "FastQC pós-fastp concluído. Resultados em: ${OUT_DIR}"
