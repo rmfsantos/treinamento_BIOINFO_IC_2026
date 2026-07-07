668a14a2b0f77631213fef22130e916ee02ff983
rmfsantos <111084824+rmfsantos@users.noreply.github.com>
2026-07-07 11:21:59 -0300

    Update 09_run_multiqc_cutadapt.sh

--- Aula_2/09_run_multiqc_cutadapt.sh
+++ Aula_2/09_run_multiqc_cutadapt.sh
@@ -5,9 +5,6 @@ ENV_NAME="orchid_qc"
 IN_DIR="results/cutadapt/fastqc"
 OUT_DIR="results/cutadapt/multiqc"
 
-echo "Ativando ambiente Conda: ${ENV_NAME}..."
-conda activate "${ENV_NAME}"
-
 echo "Criando diretório de saída do MultiQC (Cutadapt): ${OUT_DIR}..."
 mkdir -p "${OUT_DIR}"
 
